2906ca064831389f67ba2ea80d4d451124a9720a
Ricardo Faria <ricardo.faria@unipe.br>
2017-06-05 15:04:06 -0300

    Remover Registo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,6 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                         <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Registo</a></li>
                         @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
